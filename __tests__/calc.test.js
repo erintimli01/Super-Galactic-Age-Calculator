38cd51c4092730a2d1ab21fcd342ac8005ae51d2
Erin Timlin <erintimlin@gmail.com>
2023-02-03 13:16:28 -0500

    add test for venus years to account for 0 and tests pass with full line coverage

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -10,7 +10,7 @@ test('Should correctly create a Planet object with an age property', () => {
 test('Should correctly calculate a users age in Mercury years', () => {
   const planet = new Planet(32)
   expect(planet.mercuryYears()).toEqual(133.33333333333334);
-}); //passed but not full line coverage without below test
+}); //passed but not full line coverage without below test to account for 0
 
 test("Should return 0 if age is 0", () => {
   const planet = new Planet(0);
@@ -19,7 +19,12 @@ test("Should return 0 if age is 0", () => {
 
 test('Should correctly calculate a users age in Venus years', () => {
   const planet = new Planet(32);
-  expect(planet.venusYears()).toEqual(51.61290322580645);
-})
+  expect(planet.venusYears()).toEqual(51.612903225806456);
+}); //passed but not full line coverage without below test to account for 0
+
+test("Should return 0 if age is 0", () => {
+  const planet = new Planet(0);
+  expect(planet.venusYears()).toEqual(0);
+});
 
 })
--- src/js/calc.js
+++ src/js/calc.js
@@ -1,7 +1,8 @@
 export default class Planet {
-  constructor(age, mercuryAge) {
+  constructor(age, mercuryAge, venusAge) {
   this.age = age;
   this.mercuryAge = mercuryAge;
+  this.venusAge = venusAge;
 
   // thisMercuryYears = Math.floor(this.age / .24);
   }
@@ -10,6 +11,15 @@ mercuryYears() {
   let mercuryAge = this.age / .24;
   if((this.age !== 0) && (mercuryAge = this.age / .24)) {
     return mercuryAge;
+  } else {
+    return this.age;
+  }
+}
+
+venusYears() {
+  let venusAge = this.age / .62;
+  if((this.age !== 0) && (venusAge = this.age / .62)) {
+    return venusAge;
   } else {
     return this.age;
   }
