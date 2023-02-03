5c83dc647f90b1104b9898e49b87e1ee2f418a13
Erin Timlin <erintimlin@gmail.com>
2023-02-03 12:58:14 -0500

    test for mercury years passed but not full line coverage without a test to account for age=0

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,6 +1,18 @@
 export default class Planet {
-  constructor(age) {
+  constructor(age, mercuryAge) {
   this.age = age;
+  this.mercuryAge = mercuryAge;
+
+  // thisMercuryYears = Math.floor(this.age / .24);
   }
+
+mercuryYears() {
+  let mercuryAge = this.age / .24;
+  if((this.age !== 0) && (mercuryAge = this.age / .24)) {
+    return mercuryAge;
+  }
+}
+
   
+
 }
