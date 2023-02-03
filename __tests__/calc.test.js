dad0c7c9513b0e84d8f9befdfb16aa6c35cf87f7
Erin Timlin <erintimlin@gmail.com>
2023-02-03 13:17:08 -0500

    add test to calculate age in mars years

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -27,4 +27,9 @@ test("Should return 0 if age is 0", () => {
   expect(planet.venusYears()).toEqual(0);
 });
 
+test('Should correctly calculate a users age in Mars years', () => {
+  const planet = new Planet(32);
+  expect(planet.marsYears()).toEqual(51.612903225806456);
+});
+
 })
