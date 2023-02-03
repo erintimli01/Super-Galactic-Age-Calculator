dc6bb3120c7932800cf906069ec720adede9a843
Erin Timlin <erintimlin@gmail.com>
2023-02-03 13:00:57 -0500

    add test to account for 0 in mercuryYears

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -10,6 +10,11 @@ test('Should correctly create a Planet object with an age property', () => {
 test('should correctly calculate a users age in mercury years', () => {
   const planet = new Planet(32)
   expect(planet.mercuryYears()).toEqual(133.33333333333334);
-}); 
+}); //passed but not full line coverage without below test
+
+test("should return 0 if age is 0", () => {
+  const planet = new Planet(0);
+  expect(planet.mercuryYears()).toEqual(0);
+});
 
 })
